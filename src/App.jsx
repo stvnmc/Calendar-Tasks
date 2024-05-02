735ed7f1cf33f07887f7d4778988c07dfefdcd02
stvnmc <stvnmc123@gmail.com>
2024-05-02 09:21:30 -0600

    Desplegar la aplicacion en GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -10,19 +10,20 @@ import CreateRoutine from "./page/CreateRoutine";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/Calendar-Tasks/register" element={<RegisterPage />} />
-        <Route path="/Calendar-Tasks/login" element={<LoginPage />} />
+    <h1>hola</h1>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/Calendar-Tasks/register" element={<RegisterPage />} />
+    //     <Route path="/Calendar-Tasks/login" element={<LoginPage />} />
 
-        <Route element={<ProtectedRoute />}>
-          <Route path="/Calendar-Tasks" element={<Home />} />
-          <Route path="/Calendar-Tasks/month/:id1/:id2" element={<Month />} />
-          <Route path="/Calendar-Tasks/m/:id1/d/:id2/y/:id3" element={<Day />} />
-          <Route path="/Calendar-Tasks/create-routine" element={<CreateRoutine />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    //     <Route element={<ProtectedRoute />}>
+    //       <Route path="/Calendar-Tasks" element={<Home />} />
+    //       <Route path="/Calendar-Tasks/month/:id1/:id2" element={<Month />} />
+    //       <Route path="/Calendar-Tasks/m/:id1/d/:id2/y/:id3" element={<Day />} />
+    //       <Route path="/Calendar-Tasks/create-routine" element={<CreateRoutine />} />
+    //     </Route>
+    //   </Routes>
+    // </BrowserRouter>
   );
 }
 
